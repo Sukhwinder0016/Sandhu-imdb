238b26de30fd7dc11adb28cc7b24e0c14135b742
Sukhwinder0016 <158042570+Sukhwinder0016@users.noreply.github.com>
2025-04-14 05:44:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,11 +37,11 @@ LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002122152204'))
 auth_channel = environ.get('AUTH_CHANNEL', '-1002429510787')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002370487322'))
 request_channel = environ.get('REQUEST_CHANNEL', '-1002116325676') # If anyone sends a request message to your bot, you will get it in this channel.
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002116325676')) # 
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0')) # 
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+mrrgTUvDf5YyY2Q1') #Support group link ( make sure bot is admin )
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/+mrrgTUvDf5YyY2Q1")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/+dVRLYHXJztJlMmY9")
